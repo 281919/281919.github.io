8e216403f32d27b3bee115ce529d6efbd0e9d12b
alsl <xiezhisheng2016@163.com>
2019-09-08 18:54:59 +0800

    Initial commit

--- js/ajax.js
+++ js/ajax.js
@@ -3,15 +3,15 @@
 
 
 window.onload = function() {
-  // var xhr = new XMLHttpRequest();
-  // xhr.open("get", "./data/menu.text", false);
-  // xhr.send();
+  var xhr = new XMLHttpRequest();
+  xhr.open("get", "./data/menu.text", false);
+  xhr.send();
 
-  // if(xhr.status >= 200 && xhr.status < 300 || xhr.status == 304){
-  //   var menu = JSON.parse(xhr.responseText);
+  if(xhr.status >= 200 && xhr.status < 300 || xhr.status == 304){
+    var menu = JSON.parse(xhr.responseText);
     
     
-  // } else {
-  //   alert("请求无响应" + xhr.status);
-  // }
+  } else {
+    alert("请求无响应" + xhr.status);
+  }
 }
--- js/menu.js
+++ js/menu.js
@@ -1,26 +1,26 @@
-var vm = new Vue ({
-  el: "#wrap",
-  data:{
-    menuContent = []
-  },
-  mounted:function(){
-    this.$nextTick(function(){
-      this.menuView()
-    })
-   },
-   methods:{
-     menuView: function() {
-      var xhr = new XMLHttpRequest();
-      xhr.open("get", "data/menu.text", false);
-      xhr.send();
+// var vm = new Vue ({
+//   el: "#wrap",
+//   data:{
+//     menuContent = []
+//   },
+//   mounted:function(){
+//     this.$nextTick(function(){
+//       this.menuView()
+//     })
+//    },
+//    methods:{
+//      menuView: function() {
+//       var xhr = new XMLHttpRequest();
+//       xhr.open("get", "data/menu.text", false);
+//       xhr.send();
     
-      if(xhr.status >= 200 && xhr.status < 300 || xhr.status == 304){
-        var menu = JSON.parse(xhr.responseText);
-        var menuContent = JSON.parse(xhr.responseText).list;
-      } else {
-        alert("请求无响应" + xhr.status);
-      }
-     }
-   }
+//       if(xhr.status >= 200 && xhr.status < 300 || xhr.status == 304){
+//         var menu = JSON.parse(xhr.responseText);
+//         var menuContent = JSON.parse(xhr.responseText).list;
+//       } else {
+//         alert("请求无响应" + xhr.status);
+//       }
+//      }
+//    }
 
-})
+// })
