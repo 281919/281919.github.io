2433cfbcf365bc30bc1c2f4013ee5d1477614461
alsl <xiezhisheng2016@163.com>
2019-09-08 18:28:28 +0800

    Initial commit

--- js/ajax.js
+++ js/ajax.js
@@ -1,13 +1,17 @@
 
-window.onload = function() {
-  var xhr = new XMLHttpRequest();
-  xhr.open("get", "./data/menu.text", false);
-  xhr.send();
 
-  if(xhr.status >= 200 && xhr.status < 300 || xhr.status == 304){
-    var menu = JSON(xhr.responseText);
+
+
+window.onload = function() {
+  // var xhr = new XMLHttpRequest();
+  // xhr.open("get", "./data/menu.text", false);
+  // xhr.send();
+
+  // if(xhr.status >= 200 && xhr.status < 300 || xhr.status == 304){
+  //   var menu = JSON.parse(xhr.responseText);
     
-  } else {
-    alert("请求无响应" + xhr.status);
-  }
+    
+  // } else {
+  //   alert("请求无响应" + xhr.status);
+  // }
 }
